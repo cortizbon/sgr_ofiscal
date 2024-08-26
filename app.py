abcdd48cf8b248461917296bc698ec594601c6c1
Carlos Ortiz <carlosortiz@192.168.0.3>
2024-08-25 19:13:37 -0500

    push

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # seteo de la configuración de la página
 
@@ -16,12 +17,14 @@ sgr_func = pd.read_csv('datasets/sgr_func.csv')
 sgr_miner = pd.read_csv('datasets/sgr_miner_2.csv')
 sgr_hidroc = pd.read_csv('datasets/sgr_hidroc.csv')
 sgr_regal_comp = pd.read_csv('datasets/sgr_regal_tot.csv')
+sgr_proy = pd.read_csv('datasets/base_proyectos_simp.csv')
+sgr_cont = pd.read_csv('datasets/base_contratos_simp.csv')
 
 # titulo
 
 st.title("Sistema General de Regalías")
 
-tab1, tab2 = st.tabs(['Recaudo', 'Asignación'])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Recaudo', 'Asignación', 'Proyectos', 'Contratos', 'Asignación regionalizada'])
 
 # varios tabs
 
@@ -116,10 +119,8 @@ with tab1:
 
     sgr_miner_per = sgr_miner[sgr_miner['Periodo'] == per]
     fig = px.treemap(sgr_miner_per,
-                     path=[px.Constant("Total regalías mineras"),
-                           "clasificacion",
-                           "Departamento",
-                           "Municipio"],
+                     path=[px.Constant("Total recaudo minero"),
+                           "clasificacion"],
                     values='Regalías causadas')
     st.plotly_chart(fig)
 
@@ -178,3 +179,402 @@ with tab2:
     st.dataframe(inv)
     fig = px.bar(inv, x='Periodo', y='Instrucción de abono a cuenta', color='CONCEPTO 2')
     st.plotly_chart(fig)    
+
+with tab3:
+
+    st.header("Por año")
+
+    piv_sum_tot_an = sgr_proy.groupby(['AÑO APROBACIÓN'])['total_proyecto_2024'].sum().reset_index()
+    piv_sum_tot_per = sgr_proy.groupby(['Periodo'])['total_proyecto_2024'].sum().reset_index()
+    st.dataframe(piv_sum_tot_an)
+    st.dataframe(piv_sum_tot_per)
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Por año", "Por bienio"))
+
+    # Add the first line plot to the first subplot
+    fig.add_trace(go.Scatter(x=piv_sum_tot_an['AÑO APROBACIÓN'], 
+                             y=piv_sum_tot_an['total_proyecto_2024'], 
+                             mode='lines', 
+                             name='Por año'), row=1, col=1)
+
+    # Add the second line plot to the second subplot
+    fig.add_trace(go.Scatter(x=piv_sum_tot_per['Periodo'], 
+                             y=piv_sum_tot_per['total_proyecto_2024'], 
+                             mode='lines', 
+                             name='Por bienio'), row=1, col=2)
+
+    # Customize the layout
+    fig.update_layout(
+        title="Valor total de los proyectos por año y bienio",
+        template="plotly_white" # Hide legend if not needed
+    )
+
+    # Show the figure
+    st.plotly_chart(fig)
+
+    piv_count_tot_an = sgr_proy.groupby(['AÑO APROBACIÓN'])['total_proyecto_2024'].count().reset_index()
+    piv_count_tot_per = sgr_proy.groupby(['Periodo'])['total_proyecto_2024'].count().reset_index()
+    st.dataframe(piv_count_tot_an)
+    st.dataframe(piv_count_tot_per)
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Por año", "Por bienio"))
+
+    # Add the first line plot to the first subplot
+    fig.add_trace(go.Scatter(x=piv_count_tot_an['AÑO APROBACIÓN'], 
+                             y=piv_count_tot_an['total_proyecto_2024'], 
+                             mode='lines', 
+                             name='Por año'), row=1, col=1)
+
+    # Add the second line plot to the second subplot
+    fig.add_trace(go.Scatter(x=piv_count_tot_per['Periodo'], 
+                             y=piv_count_tot_per['total_proyecto_2024'], 
+                             mode='lines', 
+                             name='Por bienio'), row=1, col=2)
+
+    # Customize the layout
+    fig.update_layout(
+        title="Número de proyectos por año y bienio",
+        template="plotly_white" # Hide legend if not needed
+    )
+
+    # Show the figure
+    st.plotly_chart(fig)
+
+    piv_mean_tot_an = sgr_proy.groupby(['AÑO APROBACIÓN'])['total_proyecto_2024'].mean().reset_index()
+    piv_mean_tot_per = sgr_proy.groupby(['Periodo'])['total_proyecto_2024'].mean().reset_index()
+    st.dataframe(piv_mean_tot_an)
+    st.dataframe(piv_mean_tot_per)
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Por año", "Por bienio"))
+
+    # Add the first line plot to the first subplot
+    fig.add_trace(go.Scatter(x=piv_mean_tot_an['AÑO APROBACIÓN'], 
+                             y=piv_mean_tot_an['total_proyecto_2024'], 
+                             mode='lines', 
+                             name='Por año'), row=1, col=1)
+
+    # Add the second line plot to the second subplot
+    fig.add_trace(go.Scatter(x=piv_mean_tot_per['Periodo'], 
+                             y=piv_mean_tot_per['total_proyecto_2024'], 
+                             mode='lines', 
+                             name='Por bienio'), row=1, col=2)
+
+    # Customize the layout
+    fig.update_layout(
+        title="Valor promedio de los proyectos por año y bienio",
+        template="plotly_white" # Hide legend if not needed
+    )
+
+    # Show the figure
+    st.plotly_chart(fig)
+
+    st.header("Por año y por departamento")
+
+    st.subheader("Cantidad de proyectos")
+
+
+
+    # heatmap de número de proyectos por departamento y por año
+
+    piv_dc_an = sgr_proy.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='AÑO APROBACIÓN',
+                        values='total_proyecto_2024',
+                        aggfunc='count')
+    
+    piv_dc_per = sgr_proy.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='Periodo',
+                        values='total_proyecto_2024',
+                        aggfunc='count')
+    
+    st.dataframe(piv_dc_an)
+    fig = px.imshow(piv_dc_an, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+    st.dataframe(piv_dc_per)
+
+    fig = px.imshow(piv_dc_per, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+    
+    st.subheader("Valor total de los proyectos")
+    piv_ds_an = sgr_proy.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='AÑO APROBACIÓN',
+                        values='total_proyecto_2024',
+                        aggfunc='sum')
+    
+    piv_ds_per = sgr_proy.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='Periodo',
+                        values='total_proyecto_2024',
+                        aggfunc='sum')
+    
+    st.dataframe(piv_ds_an)
+    fig = px.imshow(piv_ds_an, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+    st.dataframe(piv_ds_per)
+    fig = px.imshow(piv_ds_per, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+
+    st.subheader("Valor promedio de cada proyecto")
+    
+    piv_dm_an = sgr_proy.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='AÑO APROBACIÓN',
+                        values='total_proyecto_2024',
+                        aggfunc='mean')
+    
+    piv_dm_per = sgr_proy.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='Periodo',
+                        values='total_proyecto_2024',
+                        aggfunc='mean')
+
+    st.dataframe(piv_dm_an)
+    fig = px.imshow(piv_dm_an, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+    st.dataframe(piv_dm_per)
+    fig = px.imshow(piv_dm_per, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+
+    # métricas con desfases
+
+    # descarga de datos
+
+    for col in ['FECHA INICIO PROGRAMACIÓN INICIAL', 'FECHA FINAL PROGRAMACIÓN INICIAL',
+           'FECHA INICIO PROGRAMACIÓN ACTUAL', 'FECHA FINAL PROGRAMACIÓN ACTUAL']:
+        sgr_proy[col] = pd.to_datetime(sgr_proy[col])
+    
+
+    fechas = sgr_proy[['DEPARTAMENTO EJECUTOR','AÑO APROBACIÓN', 'Periodo','FECHA INICIO PROGRAMACIÓN INICIAL', 'FECHA FINAL PROGRAMACIÓN INICIAL',
+           'FECHA INICIO PROGRAMACIÓN ACTUAL', 'FECHA FINAL PROGRAMACIÓN ACTUAL']]
+    
+    st.subheader("Desfase en fechas de inicio")
+    
+    fechas['DESFASE FECHA INICIO'] = fechas['FECHA INICIO PROGRAMACIÓN ACTUAL'] - fechas['FECHA INICIO PROGRAMACIÓN INICIAL']
+    a = fechas[fechas['DESFASE FECHA INICIO'] == pd.to_timedelta(0)].groupby('AÑO APROBACIÓN')['DESFASE FECHA INICIO'].count()
+    b = fechas[fechas['DESFASE FECHA INICIO'] > pd.to_timedelta(0)].groupby('AÑO APROBACIÓN')['DESFASE FECHA INICIO'].count()
+    c = fechas[fechas['DESFASE FECHA INICIO'] < pd.to_timedelta(0)].groupby('AÑO APROBACIÓN')['DESFASE FECHA INICIO'].count()
+
+    tab = pd.concat([a, b, c], axis=1)
+    tab.columns = ['A TIEMPO', 'APLAZADO', 'ADELANTADO']
+    tab = tab.div(tab.sum(axis=1), axis=0)
+    tab = tab.unstack().reset_index(name='num_proyectos')
+    tab.columns = ['cat', 'año', 'num_proyectos']
+
+    fig = px.area(tab, x='año', y='num_proyectos', color='cat')
+
+    st.plotly_chart(fig)
+
+    st.subheader("Número de proyectos con fecha de inicio aplazada por departamento")
+
+    fechas['APLAZADA'] = fechas['DESFASE FECHA INICIO'] > pd.to_timedelta(0)
+
+    piv_fi = fechas.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                                columns='AÑO APROBACIÓN',
+                                values='APLAZADA',
+                                aggfunc='mean')
+
+    fig = px.imshow(piv_fi, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+
+    fechas['DESFASE FECHA FINAL'] = fechas['FECHA FINAL PROGRAMACIÓN ACTUAL'] - fechas['FECHA FINAL PROGRAMACIÓN INICIAL']
+    a = fechas[fechas['DESFASE FECHA FINAL'] == pd.to_timedelta(0)].groupby('AÑO APROBACIÓN')['DESFASE FECHA FINAL'].count()
+    b = fechas[fechas['DESFASE FECHA FINAL'] > pd.to_timedelta(0)].groupby('AÑO APROBACIÓN')['DESFASE FECHA FINAL'].count()
+    c = fechas[fechas['DESFASE FECHA FINAL'] < pd.to_timedelta(0)].groupby('AÑO APROBACIÓN')['DESFASE FECHA FINAL'].count()
+
+    st.subheader("Desfase en fechas de finalización")
+
+    tab = pd.concat([a, b, c], axis=1)
+    tab.columns = ['A TIEMPO', 'APLAZADO', 'ADELANTADO']
+    tab = tab.div(tab.sum(axis=1), axis=0)
+    tab = tab.unstack().reset_index(name='num_proyectos')
+    tab.columns = ['cat', 'año', 'num_proyectos']
+
+    fig = px.area(tab, x='año', y='num_proyectos', color='cat')
+
+    st.plotly_chart(fig)
+
+
+    st.subheader("Número de proyectos con fecha de finalización aplazada por departamento")
+
+    fechas['APLAZADA'] = fechas['DESFASE FECHA FINAL'] > pd.to_timedelta(0)
+
+    piv_fi = fechas.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                                columns='AÑO APROBACIÓN',
+                                values='APLAZADA',
+                                aggfunc='mean')
+
+    fig = px.imshow(piv_fi, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+
+with tab4:
+    st.header("Por año")
+
+    piv_sum_tot_an = sgr_cont.groupby(['AÑO APROBACIÓN'])['total_contrato_2024'].sum().reset_index()
+    piv_sum_tot_per = sgr_cont.groupby(['Periodo'])['total_contrato_2024'].sum().reset_index()
+    st.dataframe(piv_sum_tot_an)
+    st.dataframe(piv_sum_tot_per)
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Por año", "Por bienio"))
+
+    # Add the first line plot to the first subplot
+    fig.add_trace(go.Scatter(x=piv_sum_tot_an['AÑO APROBACIÓN'], 
+                             y=piv_sum_tot_an['total_contrato_2024'], 
+                             mode='lines', 
+                             name='Por año'), row=1, col=1)
+
+    # Add the second line plot to the second subplot
+    fig.add_trace(go.Scatter(x=piv_sum_tot_per['Periodo'], 
+                             y=piv_sum_tot_per['total_contrato_2024'], 
+                             mode='lines', 
+                             name='Por bienio'), row=1, col=2)
+
+    # Customize the layout
+    fig.update_layout(
+        title="Valor total de los contratos por año y bienio",
+        template="plotly_white" # Hide legend if not needed
+    )
+
+    # Show the figure
+    st.plotly_chart(fig)
+
+    piv_count_tot_an = sgr_cont.groupby(['AÑO APROBACIÓN'])['total_contrato_2024'].count().reset_index()
+    piv_count_tot_per = sgr_cont.groupby(['Periodo'])['total_contrato_2024'].count().reset_index()
+    st.dataframe(piv_count_tot_an)
+    st.dataframe(piv_count_tot_per)
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Por año", "Por bienio"))
+
+    # Add the first line plot to the first subplot
+    fig.add_trace(go.Scatter(x=piv_count_tot_an['AÑO APROBACIÓN'], 
+                             y=piv_count_tot_an['total_contrato_2024'], 
+                             mode='lines', 
+                             name='Por año'), row=1, col=1)
+
+    # Add the second line plot to the second subplot
+    fig.add_trace(go.Scatter(x=piv_count_tot_per['Periodo'], 
+                             y=piv_count_tot_per['total_contrato_2024'], 
+                             mode='lines', 
+                             name='Por bienio'), row=1, col=2)
+
+    # Customize the layout
+    fig.update_layout(
+        title="Número de contratos por año y bienio",
+        template="plotly_white" # Hide legend if not needed
+    )
+
+    # Show the figure
+    st.plotly_chart(fig)
+
+    piv_mean_tot_an = sgr_cont.groupby(['AÑO APROBACIÓN'])['total_contrato_2024'].mean().reset_index()
+    piv_mean_tot_per = sgr_cont.groupby(['Periodo'])['total_contrato_2024'].mean().reset_index()
+    st.dataframe(piv_mean_tot_an)
+    st.dataframe(piv_mean_tot_per)
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Por año", "Por bienio"))
+
+    # Add the first line plot to the first subplot
+    fig.add_trace(go.Scatter(x=piv_mean_tot_an['AÑO APROBACIÓN'], 
+                             y=piv_mean_tot_an['total_contrato_2024'], 
+                             mode='lines', 
+                             name='Por año'), row=1, col=1)
+
+    # Add the second line plot to the second subplot
+    fig.add_trace(go.Scatter(x=piv_mean_tot_per['Periodo'], 
+                             y=piv_mean_tot_per['total_contrato_2024'], 
+                             mode='lines', 
+                             name='Por bienio'), row=1, col=2)
+
+    # Customize the layout
+    fig.update_layout(
+        title="Valor promedio de los contrato por año y bienio",
+        template="plotly_white" # Hide legend if not needed
+    )
+
+    # Show the figure
+    st.plotly_chart(fig)
+
+    st.header("Por año y por departamento")
+
+    st.subheader("Cantidad de contratos")
+
+
+
+    # heatmap de número de proyectos por departamento y por año
+
+    piv_dc_an = sgr_cont.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='AÑO APROBACIÓN',
+                        values='total_contrato_2024',
+                        aggfunc='count')
+    
+    piv_dc_per = sgr_cont.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='Periodo',
+                        values='total_contrato_2024',
+                        aggfunc='count')
+    
+    st.dataframe(piv_dc_an)
+    fig = px.imshow(piv_dc_an, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+    st.dataframe(piv_dc_per)
+
+    fig = px.imshow(piv_dc_per, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+    
+    st.subheader("Valor total de los proyectos")
+    piv_ds_an = sgr_cont.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='AÑO APROBACIÓN',
+                        values='total_contrato_2024',
+                        aggfunc='sum')
+    
+    piv_ds_per = sgr_cont.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='Periodo',
+                        values='total_contrato_2024',
+                        aggfunc='sum')
+    
+    st.dataframe(piv_ds_an)
+    fig = px.imshow(piv_ds_an, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+    st.dataframe(piv_ds_per)
+    fig = px.imshow(piv_ds_per, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+
+    st.subheader("Valor promedio de cada contrato")
+    
+    piv_dm_an = sgr_cont.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='AÑO APROBACIÓN',
+                        values='total_contrato_2024',
+                        aggfunc='mean')
+    
+    piv_dm_per = sgr_cont.pivot_table(index='DEPARTAMENTO EJECUTOR',
+                        columns='Periodo',
+                        values='total_contrato_2024',
+                        aggfunc='mean')
+
+    st.dataframe(piv_dm_an)
+    fig = px.imshow(piv_dm_an, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+    st.dataframe(piv_dm_per)
+    fig = px.imshow(piv_dm_per, text_auto=True, aspect="auto")
+
+    st.plotly_chart(fig)
+
+    depto = st.selectbox("Seleccione un departamento: ", sgr_cont['DEPARTAMENTO EJECUTOR'].unique().tolist())
+    fil_depto = sgr_cont[sgr_cont['DEPARTAMENTO EJECUTOR'] == depto]
+    piv_depto = fil_depto.groupby('NOMBRE CONTRATISTA')['total_contrato_2024'].sum().sort_values(ascending=False).reset_index().head(10)
+
+    fig = px.bar(piv_depto, x='NOMBRE CONTRATISTA',y='total_contrato_2024')
+    st.plotly_chart(fig)
+
+    piv_depto = fil_depto.groupby('NOMBRE CONTRATISTA')['total_contrato_2024'].count().sort_values(ascending=False).reset_index().head(10)
+
+    fig = px.bar(piv_depto, x='NOMBRE CONTRATISTA',y='total_contrato_2024')
+    st.plotly_chart(fig)
+
